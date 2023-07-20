2078a9dc4b920fe77639418bf9d7bb6be85819f2
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-07-20 12:22:36 -0700

    Init checkin of demo

---
+++ renderer.js
@@ -0,0 +1,50 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const { SerialPort } = require('serialport')
+const tableify = require('tableify')
+
+async function listSerialPorts() {
+  await SerialPort.list().then((ports, err) => {
+    if(err) {
+      document.getElementById('error').textContent = err.message
+      return
+    } else {
+      document.getElementById('error').textContent = ''
+    }
+    console.log('ports', ports);
+
+    if (ports.length === 0) {
+      document.getElementById('error').textContent = 'No ports discovered'
+    }
+
+    tableHTML = tableify(ports)
+    document.getElementById('ports').innerHTML = tableHTML
+  })
+}
+
+function listPorts() {
+  listSerialPorts();
+  setTimeout(listPorts, 2000);
+}
+
+// Set a timeout that will check for new serialPorts every 2 seconds.
+// This timeout reschedules itself.
+setTimeout(listPorts, 2000);
+
+listSerialPorts()
+
+const port = new SerialPort({ path: 'COM15', baudRate: 57600 })
+
+port.write('f5\r', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  }
+  console.log('message written')
+})
+
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
