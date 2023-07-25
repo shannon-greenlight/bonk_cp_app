72d7d75684b1c6769818eaeb7e64ff441db5451e
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-07-25 11:16:06 -0700

    Resizes container

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
-const url = require("url");
-const fs = require("fs");
+const { app, BrowserWindow, ipcMain } = require("electron")
+const path = require("path")
+const url = require("url")
+const fs = require("fs")
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let win
 
 async function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     autoHideMenuBar: true,
-    width: 1133,
-    height: 1015,
+    width: 1116,
+    height: 1006,
     webPreferences: {
       nodeIntegration: true, // is default value after Electron v5
       contextIsolation: false, // protect against prototype pollution
@@ -20,10 +20,10 @@ async function createWindow() {
       // preload: path.join(__dirname, "preload.js") // use a preload script
       // preload: path.join(app.getAppPath(), 'preload.js')
     },
-  });
+  })
 
   // Load app
-  win.loadFile(path.join(__dirname, "index.html"));
+  win.loadFile(path.join(__dirname, "index.html"))
 
   // rest of code..
   // Open the DevTools.
@@ -34,8 +34,8 @@ async function createWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
-  });
+    win = null
+  })
 }
 
 // let mainWindow
@@ -84,8 +84,8 @@ async function createWindow() {
 // })
 
 app.whenReady().then(() => {
-  createWindow();
-});
+  createWindow()
+})
 
 // app.on('ready', createWindow)
 
@@ -93,16 +93,16 @@ app.whenReady().then(() => {
 app.on("window-all-closed", function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  app.quit();
-});
+  app.quit()
+})
 
 app.on("activate", function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
