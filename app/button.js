e263d85354c13993dcbbfba85a9329399f4997de
shannon-greenlight <shannon@greenlightdevelopment.com>
2024-04-28 12:47:07 -0700

    Adds meaningful titles to buttons.

--- app/button.js
+++ app/button.js
@@ -3,12 +3,33 @@ class Button {
     this.id = id
     this.active_color = active_color
     this.selected_color = selected_color
+    this.state = false
+    this.trigger = {}
   }
 
   set(data) {
+    this.state = data === "ON"
     const selected = this.trigger.outputs !== 0
     const inactive_color = selected ? this.selected_color : "#aaa"
-    $(`#${this.id}`).css("background-color", data === "ON" ? this.active_color : inactive_color)
+
+    let title = "Click "
+    if (this.id === "group") {
+      title += this.state ? "to Deactivate" : "to Activate"
+      title += " group."
+    } else {
+      const is_disabled = $(`#${this.id}`).prop("disabled")
+      dbugger.print(`${this.id} is_disabled: ${is_disabled}`, false)
+      if (is_disabled) {
+        title += "an Output to Enable Trigger."
+      } else {
+        title += this.state ? " to Retrigger" : " to Trigger"
+        title += " Outputs."
+      }
+    }
+
+    $(`#${this.id}`)
+      .css("background-color", this.state ? this.active_color : inactive_color)
+      .attr("title", title)
   }
 }
 
