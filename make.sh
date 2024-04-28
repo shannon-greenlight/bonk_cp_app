012341e778ed86ca30dcfbe533625ae04c7a1ef9
shannon-greenlight <shannon@greenlightdevelopment.com>
2024-04-28 12:50:14 -0700

    Fixes make.

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
-npx electron-forge make
-
+src_dir="./out/make/zip/darwin/arm64"
+rm -f $src_dir/*
 rm ./out/mac_bonk_cp_app.zip
 # cp ./out/make/zip/darwin/arm64/mac_bonk_cp_app-darwin-arm64-3.2.1.zip ./out/mac_bonk_cp_app.zip
-src_dir="./out/make/zip/darwin/arm64"
+
+npx electron-forge make
+
 src_file=($src_dir/*)
 filename=$(basename "$src_file")
 cp "$src_file" ./out/mac_bonk_cp_app.zip
